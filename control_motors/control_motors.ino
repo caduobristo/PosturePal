00f5f088119c801e23ea9e1ea1e3f7a31008613a
Carlos Eduardo Obristo <carlos.eduardo@policorp.com.br>
2025-11-11 14:25:27 -0300

    Motores pararem com sensor de obstáculo

--- control_motors/control_motors.ino
+++ control_motors/control_motors.ino
@@ -2,73 +2,69 @@
 
 BluetoothSerial SerialBT;
 
-// Pinos motores
-const int IN1 = 25;
-const int IN2 = 26;
-const int IN3 = 27;
-const int IN4 = 14;
-const int ENA = 4;
-const int ENB = 5;
+// Pinos dos motores
+const int IN1 = 25, IN2 = 26, IN3 = 27, IN4 = 14;
+const int ENA = 4, ENB = 5;
 
 // PWM
-const int freq = 20000;
-const int channelA = 0;
-const int channelB = 1;
-const int resolution = 8;
+const int freq = 20000, channelA = 0, channelB = 1, resolution = 8;
 
-// Encoder rotativo
+// Encoder
 const int CLK = 32;
-const int DT  = 33;
-
 volatile int pulseCount = 0;
 unsigned long lastTime = 0;
-float rpm = 0.0;
 
-// Interrupção: detecta bordas de subida no CLK
-void IRAM_ATTR encoderISR() {
-  pulseCount++;
-}
+// Sensores IR
+const int IR_LEFT_DO = 34;
+const int IR_RIGHT_DO = 35;
+const bool IR_ACTIVE_LOW = true;
+const uint8_t IR_HITS_TO_STOP = 2;
+uint8_t irHitsLeft = 0, irHitsRight = 0;
+
+// Estado de direção e movimento
+bool indoFrente = true;
+bool andando = false;
+unsigned long startMoveTime = 0;
+unsigned long moveDuration = 11000;
+
+// Parâmetros físicos
+const float PULSOS_POR_ROTACAO = 3800.0;
+const float DIAMETRO_RODA_CM = 6.5;
+const float CIRCUNFERENCIA_CM = 3.1416 * DIAMETRO_RODA_CM;
+const unsigned long TEMPO_PARADA_MS = 1000;
+const uint8_t MIN_SAMPLES_BEFORE_STOP = 1;
+const float VEL_MIN_CM_S = 2.0;
+
+// Controle de velocidade
+unsigned long ultimaVelocidadeAtiva = 0;
+uint8_t samplesSinceStart = 0, zeroSamples = 0;
+
+// Interrupção do encoder
+void IRAM_ATTR encoderISR() { pulseCount++; }
 
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("PosturePal");
   Serial.println("Bluetooth iniciado!");
 
-  // Motores
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
   ledcSetup(channelA, freq, resolution);
   ledcSetup(channelB, freq, resolution);
   ledcAttachPin(ENA, channelA);
   ledcAttachPin(ENB, channelB);
 
-  // Encoder
+  pinMode(IR_LEFT_DO, INPUT);
+  pinMode(IR_RIGHT_DO, INPUT);
+
   pinMode(CLK, INPUT_PULLUP);
-  pinMode(DT, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(CLK), encoderISR, RISING);
 
-  off();
+  frear();
 }
 
-unsigned long moveDuration = 5000;
-
-// parâmetros físicos
-const float PULSOS_POR_ROTACAO = 3800.0;
-const float DIAMETRO_RODA_CM = 6.5;
-const float CIRCUNFERENCIA_CM = 3.1416 * DIAMETRO_RODA_CM;
-
-// tempo máximo sem movimento antes de parar
-const unsigned long TEMPO_PARADA_MS = 1000;
-
-bool andando = false;
-unsigned long startMoveTime = 0;
-unsigned long ultimaVelocidadeAtiva = 0;
-
 void loop() {
-  // Verifica comandos Bluetooth
+  // Comandos Bluetooth
   if (SerialBT.available()) {
     String comando = SerialBT.readStringUntil('\n');
     comando.trim();
@@ -77,44 +73,68 @@ void loop() {
     Serial.print("Comando recebido: ");
     Serial.println(comando);
 
-    if (comando == "frente") iniciarFrente();
-    else if (comando == "re") iniciarRe();
-    else if (comando == "off") off();
+    if (comando == "frente") {
+      if (indoFrente) iniciarFrente();
+      else iniciarRe();
+      indoFrente = !indoFrente;
+    } 
+    else if (comando == "frear") {
+      frear();
+    }
   }
 
-  // Se estiver andando, verifica se passou o tempo de movimento
+  // Tempo máximo de movimento
   if (andando && millis() - startMoveTime >= moveDuration) {
-    off();
+    frear();
   }
 
-  // Medição da velocidade a cada 1 segundo
+  // Verificação de sensores IR
+  if (andando) {
+    int l = digitalRead(IR_LEFT_DO);
+    int r = digitalRead(IR_RIGHT_DO);
+    bool leftTrig  = IR_ACTIVE_LOW ? (l == LOW)  : (l == HIGH);
+    bool rightTrig = IR_ACTIVE_LOW ? (r == LOW)  : (r == HIGH);
+
+    if (leftTrig && ++irHitsLeft >= IR_HITS_TO_STOP) {
+      SerialBT.println("parou_left");
+      frear();
+      irHitsLeft = irHitsRight = 0;
+    } else if (!leftTrig) irHitsLeft = 0;
+
+    if (andando && rightTrig && ++irHitsRight >= IR_HITS_TO_STOP) {
+      SerialBT.println("parou_right");
+      frear();
+      irHitsLeft = irHitsRight = 0;
+    } else if (!rightTrig) irHitsRight = 0;
+  }
+
+  // Medição de velocidade
   unsigned long now = millis();
   if (now - lastTime >= 1000) {
     lastTime = now;
-
+    noInterrupts();
     int pulses = pulseCount;
     pulseCount = 0;
+    interrupts();
 
-    // calcula RPM e velocidade linear
-    rpm = (float)pulses / PULSOS_POR_ROTACAO * 60.0;
+    float rpm = (float)pulses / PULSOS_POR_ROTACAO * 60.0;
     float rps = rpm / 60.0;
     float vel_cm_s = rps * CIRCUNFERENCIA_CM;
 
-    // envia pelo serial e bluetooth
-    String msg = "Pulsos: " + String(pulses) +
-                 " | RPM: " + String(rpm, 1) +
-                 " | Velocidade: " + String(vel_cm_s, 1) + " cm/s";
-    Serial.println(msg);
-    SerialBT.println(msg);
-
-    // verifica se deve parar
     if (andando) {
-      if (vel_cm_s > 2.0) {
-        ultimaVelocidadeAtiva = millis(); // está se movendo
-      } else if (millis() - ultimaVelocidadeAtiva > TEMPO_PARADA_MS) {
-        Serial.println("Velocidade zero detectada, parando motores!");
-        SerialBT.println("parou");
-        off();
+      samplesSinceStart++;
+      if (samplesSinceStart <= MIN_SAMPLES_BEFORE_STOP) {
+        if (vel_cm_s > VEL_MIN_CM_S) ultimaVelocidadeAtiva = millis();
+        zeroSamples = 0;
+      } else {
+        if (vel_cm_s > VEL_MIN_CM_S) {
+          ultimaVelocidadeAtiva = millis();
+          zeroSamples = 0;
+        } else if (++zeroSamples >= 2 &&
+                   (millis() - ultimaVelocidadeAtiva > TEMPO_PARADA_MS)) {
+          SerialBT.println("parou");
+          frear();
+        }
       }
     }
   }
@@ -122,38 +142,42 @@ void loop() {
 
 void iniciarFrente() {
   Serial.println("Movendo para frente");
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  ledcWrite(channelA, 255);
-  ledcWrite(channelB, 255);
+  digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+  ledcWrite(channelA, 255); ledcWrite(channelB, 255);
+
   andando = true;
+  irHitsLeft = irHitsRight = 0;
   startMoveTime = millis();
+  samplesSinceStart = 0;
+  zeroSamples = 0;
+  ultimaVelocidadeAtiva = millis();
 }
 
 void iniciarRe() {
-  Serial.println("Ré");
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  ledcWrite(channelA, 255);
-  ledcWrite(channelB, 255);
+  Serial.println("Movendo para trás");
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
+  ledcWrite(channelA, 255); ledcWrite(channelB, 255);
+
   andando = true;
+  irHitsLeft = irHitsRight = 0;
   startMoveTime = millis();
+  samplesSinceStart = 0;
+  zeroSamples = 0;
+  ultimaVelocidadeAtiva = millis();
 }
 
-void off() {
+void frear() {
   if (andando) {
     andando = false;
     SerialBT.println("parou");
   }
-  Serial.println("Motores parados");
+  Serial.println("Freio ativo!");
   ledcWrite(channelA, 0);
   ledcWrite(channelB, 0);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, HIGH);
 }
